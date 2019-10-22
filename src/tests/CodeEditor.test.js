c91b8d4e0d876a51b60dc2248fbe1622848eb6bd
Paul Gamble <pgamble@gmail.com>
2019-10-22 17:34:03 -0400

    ✅  - failing on text between div tags
    Trying to understand testing better. Something to reference: https://stackoverflow.com/questions/42303820/assert-react-component-children-existence-absence-using-jestjs | https://www.polvara.me/posts/five-things-you-didnt-know-about-testing-library/

--- src/tests/CodeEditor.test.js
+++ src/tests/CodeEditor.test.js
@@ -28,6 +28,21 @@ describe('Code Editor renders as expected with label and icon, status label, clo
       const label = ce.firstChild;
       expect(label.tagName).toBe('DIV');
       expect(label.classList).toContain('ui', 'scale', 'down', 'right', 'very', 'wide', 'sidebar');
+
+      // first child should contain 2 nodes (top menu, editor) with expected attributes and classes
+      expect(label.childNodes.length).toEqual(2);
+      const [menu] = label.childNodes;
+      expect(menu.tagName).toBe('DIV');
+      expect(menu.classList).toContain('ui', 'massive', 'borderless', 'top', 'attached', 'menu');
+      expect(menu.nodeName).toBe('DIV');
+      
+      // menu should have a left label, status mext, and a close button 
+      expect(menu.childElementCount).toEqual(3);
+      expect(menu.firstChild.tagName).toEqual('DIV');
+      expect(menu.firstChild.classList).toContain('left', 'item');
+      expect(menu.firstChild.childElementCount).toBe(1);
+      expect(menu.firstChild.firstChild.classList).toContain('edit', 'icon')
+      expect(menu.firstChild.firstChild.nodeValue).toContain('Code Editor')
   });
 
 
