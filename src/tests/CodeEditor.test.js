5bf552860fc9665d39ee630bc8fe864a0cf6668f
Paul Gamble <pgamble@gmail.com>
2020-01-27 22:07:54 -0500

    :white_check_mark:  - setProps should be working
    Subscribed to https://github.com/airbnb/enzyme/issues/2197; there appears to be other ppl experience the same issue of not seeing componentDidUpdate fired.

--- src/tests/CodeEditor.test.js
+++ src/tests/CodeEditor.test.js
@@ -7,8 +7,6 @@ import { Provider } from 'react-redux';
 import store from '../store';
 import CodeEditor from '../components/Tools/CodeEditor';
 import AceEditor from 'react-ace';
-import renderer from 'react-test-renderer';
-import * as sinon from "sinon";
 import { configure, mount, shallow } from 'enzyme';
 import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
@@ -41,20 +39,22 @@ describe('Test updating', () => {
   test("Did Update", () => {
     //TO-DO: refactor so there's not so many nested const; reduce testing time
     const startValue = "start value";
-    const wrapper = shallow(<Provider store={store}><CodeEditor><AceEditor/></CodeEditor></Provider>).find(AceEditor);
-    console.log(wrapper.props()); 
-    console.log(wrapper.prop('name')); 
-    // Read set value
-  
-    //let editor = wrapper.props();
-    //expect(editor.to.equal(null);
+    
+    const wrapper = mount(<Provider store={store}><CodeEditor><AceEditor /></CodeEditor></Provider>);
+    let editorValue = wrapper.find(AceEditor).prop('value')
+    //wrapper.find(AceEditor).setState({ 'value' : 'bar' });
+    //wrapper.update();
+    console.log(typeof editorValue); 
 
-    // Now trigger the componentDidUpdate
-    //const newValue = "updated value";
-    //wrapper.setProps({ value: newValue });
-    //editor = wrapper.instance().editor;
-    //expect(editor.getValue()).to.equal(newValue);    
-
+    let obj = JSON.parse(editorValue);
+    console.log(obj);
+    console.log('name: ' +  obj.header.name); 
+    obj.header.name = 'Kobe Bryant';
+    console.log('name: ' + obj.header.name); 
+    editorValue = JSON.stringify(obj);
+    console.log(editorValue);
+    wrapper.find(AceEditor).setState({ 'value': editorValue });
+    wrapper.update();
   });
 
 
